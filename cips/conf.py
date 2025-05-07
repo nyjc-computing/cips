a098926e6d3fea30f9eb266afb42d24a62592d1c
JS Ng <ngjunsiang@gmail.com>
2025-05-07 02:59:51 +0000

    [dev] add myst-parser for markdown support

--- cips/conf.py
+++ cips/conf.py
@@ -13,7 +13,9 @@ author = 'Nanyang System Developers'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "myst_parser",  # for markdown support
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
